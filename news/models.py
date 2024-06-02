22762dd228caaa84b4c3ca012fced1a2756049d8
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2024-06-02 14:52:58 +0300

    feat: created Django setup

---
+++ news/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Title')
+    content = models.TextField(blank=True, verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Photo')
+    is_published = models.BooleanField(default=True, verbose_name='Is published?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Category')
+
+    def my_func(self):
+        return 'Hello from modal'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News list'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Title')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category list'
+        ordering = ['title']
