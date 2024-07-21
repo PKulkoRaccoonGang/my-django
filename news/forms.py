f3e397ebca2b461d176d00f7fbef3acb666a5706
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-07-21 22:00:01 +0300

    feat: registration and login

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,64 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from jazzmin.templatetags.jazzmin import User
+
 from .models import News
 
 import re
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'class': 'form-control',
+        }
+        ))
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form-control',
+        }
+        ))
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'class': 'form-control',
+        }
+        ))
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form-control',
+        }
+        ))
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form-control',
+        }
+        ))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
+        'placeholder': 'Email',
+        'class': 'form-control',
+    }))
+
+    class Meta:
+        model = User
+        fields = {
+            'username': forms.TextInput(attrs={'class': 'form-control mb-2'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mb-2'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control mb-2'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control mb-2'}),
+        }
 
 
 class NewsForm(forms.ModelForm):
